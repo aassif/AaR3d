45f90a8a9c6205664fdefeced915d25fcd361066
Aa! <aassif.benassarou@univ-reims.fr>
2012-02-10 15:01:44 +0100

    Oubli de "AaR3d.h" réparé !

---
+++ include/AaR3d.h
@@ -0,0 +1,18 @@
+#ifndef __AA_R3D__
+#define __AA_R3D__
+
+#ifdef _WIN32
+  #ifdef AaR3d_EXPORTS // Merci CMake :)
+    #define AA_R3D_API    __declspec(dllexport)
+    #define AA_R3D_EXTERN
+  #else
+    #define AA_R3D_API    __declspec(dllimport)
+    #define AA_R3D_EXTERN extern
+  #endif
+#else
+  #define AA_R3D_API
+  #define AA_R3D_EXTERN
+#endif
+
+#endif // __AA_R3D__
+
